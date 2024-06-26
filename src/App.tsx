3db173596b203e11b91df42cba11c69ae8065d11
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-26 13:35:35 +0600

    increment decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
-import './App.css'
 
 function App() {
 
   return (
-    <>
-     <p className='text-2xl text-red-500 text-center'>React + Redux + TS </p>
-    </>
+    < div className="h-screen w-full flex justify-center items-center">
+      <div className="flex p-10 border border-purple-400 rounded-md bg-slate-100">
+        <button className="text-white text-xl bg-green-500 px-3 py-2 rounded-lg">increment</button>
+        <h2 className="text-2xl mx-10">0</h2>
+        <button className="text-white text-xl bg-red-500 px-3 py-2 rounded-lg">decrement</button>
+      </div>
+    </div>
   )
 }
 

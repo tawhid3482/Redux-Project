6c39c2ddf0c1b57d6548d67b9306e5d9a6f96f72
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-27 06:53:57 +0600

    custom logger

---
+++ src/redux/middleware/logger.ts
@@ -0,0 +1,7 @@
+const logger = (state)=>(next)=>(action)=>{
+    console.log('Current state =>', state.getState())
+    console.log('Action', action)
+    next(action)
+    console.log('Next state =>', state.getState())
+}
+export default logger;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './features/counterSlice'
-import logger from 'redux-logger'
+import logger from "./middleware/logger";
 export const store = configureStore({
   reducer: {
     counter: counterReducer

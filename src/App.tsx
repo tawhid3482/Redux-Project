aa20e3e7f3d6abf1a0d6e532c6bf4b282991a581
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-27 07:32:34 +0600

    disable enable is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import { decrement, increment, incrementByValue } from "./redux/features/counterSlice"
-import { useAppDispatch, useAppSelector } from "./redux/hook"
-
+import { decrement, increment, incrementByValue } from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const {count}= useAppSelector((state )=> state.counter)
-  const dispatch = useAppDispatch()
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
 
   return (
-    < div className="h-screen w-full flex justify-center items-center">
-      <div className="flex p-10 border border-purple-400 rounded-md bg-slate-100">
-        <button onClick={() => dispatch(increment())} className="text-white text-xl bg-green-500 px-3 py-2 rounded-lg mr-5">increment</button>
+    <div className="">
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="flex p-10 border border-purple-400 rounded-md bg-slate-100">
+          <button 
+            onClick={() => dispatch(increment())} 
+            className="text-white text-xl bg-green-500 px-3 py-2 rounded-lg mr-5"
+          >
+            increment
+          </button>
 
-        <button onClick={() => dispatch(incrementByValue(5))} className="text-white text-xl bg-green-500 px-3 py-2 rounded-lg ">incrementByValue</button>
+          <button 
+            onClick={() => dispatch(incrementByValue(5))} 
+            className="text-white text-xl bg-green-500 px-3 py-2 rounded-lg"
+          >
+            incrementByValue
+          </button>
 
-        <h2 className="text-2xl mx-10">{count}</h2>
+          <h2 className="text-2xl mx-10">{count}</h2>
 
-        <button onClick={()=> dispatch(decrement())} className="text-white text-xl bg-red-500 px-3 py-2 rounded-lg">decrement</button>
+          <button 
+            onClick={() => dispatch(decrement())} 
+            className="text-white text-xl bg-red-500 px-3 py-2 rounded-lg"
+          >
+            decrement
+          </button>
+        </div>
       </div>
+      {count > 5 && (
+        <div 
+          className="box w-96 h-40 text-center ml-28 flex justify-center items-center border border-red-700"
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
